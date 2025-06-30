50f48aa35e2a37f92de36d1f7be9a19c6dcc72f6
RJaqueDev <raul.jaque.developer@gmail.com>
2025-06-29 23:54:20 -0400

    Se conecta exitosamente a la base de datos

--- db/mysql.js
+++ db/mysql.js
@@ -1,12 +1,13 @@
-// Configuración de conexión a MySQL usando mysql2
+// Configuración de conexión a MySQL usando mysql2 y variables de entorno
 const mysql = require("mysql2/promise");
+require("dotenv").config(); // Carga las variables de entorno desde un archivo .env
 
 // Crea un pool de conexiones para reutilizar conexiones y mejorar el rendimiento
 const pool = mysql.createPool({
-  host: "https://www.omnidev.cl", // Cambia por tu host
-  user: "omnidevc_rjaquedev", // Cambia por tu usuario
-  password: "TheStrokes181", // Cambia por tu contraseña
-  database: "omnidevc_gastos-app", // Cambia por el nombre de tu base de datos
+  host: process.env.DB_HOST, // Host desde variables de entorno
+  user: process.env.DB_USER, // Usuario desde variables de entorno
+  password: process.env.DB_PASS, // Contraseña desde variables de entorno
+  database: process.env.DB_NAME, // Nombre de la base de datos desde variables de entorno
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const routes = require('./routes/index.js');
 // Crea una instancia de la aplicación Express
 const app = express();
 
+//Realizar prueba de conexión a la base de datos
+const db = require('./db/mysql.js');
+db.getConnection()
+  .then(connection => {
+    console.log('Conexión a la base de datos exitosa');
+    connection.release(); // Libera la conexión después de la prueba
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
+
 // Habilita CORS para permitir solicitudes de otros orígenes
 app.use(cors());
 
